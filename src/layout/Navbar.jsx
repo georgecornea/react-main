dbf81f268edb8b177c2af6c2a827bd1c9d9d6037
George Cornea <george.cornea@outlook.com>
2023-04-19 20:18:17 -0700

    styled Navbar component

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,14 +1,34 @@
 import { Link } from '@tanstack/react-location';
+import styles from './Navbar.module.css';
+import gcorneaLogo from '../assets/gcornea.png';
 
 export const Navbar = () => {
+  const activeLink = () => {
+    return {
+      style: {
+        borderBottom: '2px solid #fff',
+      },
+    };
+  };
+
   return (
-    <div>
-      <div>Logo</div>
+    <div className={styles.navbar}>
+      <Link to='/'>
+        <img src={gcorneaLogo} alt='gcornea logo' />
+      </Link>
       <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/react'>React</Link>
-        <Link to='/cplus'>C++</Link>
-        <Link to='/about'>About</Link>
+        <Link to='/' className={styles.link} getActiveProps={activeLink}>
+          Home
+        </Link>
+        <Link to='/react' className={styles.link} getActiveProps={activeLink}>
+          React
+        </Link>
+        <Link to='/cplus' className={styles.link} getActiveProps={activeLink}>
+          C++
+        </Link>
+        <Link to='/about' className={styles.link} getActiveProps={activeLink}>
+          About
+        </Link>
       </nav>
     </div>
   );
